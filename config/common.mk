a95aa021b37506542f0b1fd0fca054c09b07123e
Arseniy Graur <argraur@gmail.com>
2020-01-24 14:08:14 +0000

    vendor: Use Pixel charger animation
    Squashed with the following:
    
    Author: Arseniy Graur <argraur@gmail.com>
    Date:   Fri Jan 24 14:08:14 2020 +0000
    
        vendor: Use Pixel charger animation
    
        Change-Id: I89ac5aa6c4dec0ded4524e94dc07fd268690b5f4
    
    Author: chrisw444 <wandersonrodriguesf1@gmail.com>
    Date:   Sat Jan 22 19:21:46 2022 +0000
    
        vendor: charger: Update Pixel charger animation from SQ1D.220105.007
    
        Change-Id: I75b1fcd9580a7961cb64620c828a58e5977f2840
    
    Author: Tim Zimmermann <tim@linux4.de>
    Date:   Thu Sep 14 16:38:18 2023 +0530
    
        vendor: Support charger on vendor
    
        Change-Id: Id402ddeae8e43392d0a9f505e5cf4c162231d99f
    
    Co-authored-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Co-authored-by: Tim Zimmermann <tim@linux4.de>

--- config/common.mk
+++ config/common.mk
@@ -78,6 +78,12 @@ PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 # Disable vendor restrictions
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    product_charger_res_images \
+    product_charger_res_images_vendor
+
 ifeq ($(FAOSP_BUILDTYPE), OFFICIAL)
 # FAOSP packages
 PRODUCT_PACKAGES += \
