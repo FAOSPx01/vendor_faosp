67d001a03a8851ef1e576d613b9388bbfb9fa1e1
nift4 <nift4@protonmail.com>
2024-08-14 16:24:35 +0800

    config: Remove useless addon.d file
    * Avoid detection as custom ROM
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/common.mk
+++ config/common.mk
@@ -33,11 +33,7 @@ endif
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/faosp/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/faosp/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/faosp/prebuilt/common/bin/50-faosp.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-faosp.sh
-
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-    system/addon.d/50-faosp.sh
+    vendor/faosp/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions
 
 ifneq ($(strip $(AB_OTA_PARTITIONS) $(AB_OTA_POSTINSTALL_CONFIG)),)
 PRODUCT_COPY_FILES += \
