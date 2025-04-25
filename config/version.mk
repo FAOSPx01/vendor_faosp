LineageOS/android_vendor_lineage

config: Long live ro.modversion
Change-Id: I4acb0253ee8304c6ab2c3719e1d802e7f5fb6dbf

config/version.mk
@@ -42,5 +42,4 @@ PRODUCT_SYSTEM_PROPERTIES += \
     ro.lineage.version=$(LINEAGE_VERSION) \
     ro.lineage.display.version=$(LINEAGE_DISPLAY_VERSION) \
     ro.lineage.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
-    ro.lineage.releasetype=$(LINEAGE_BUILDTYPE) \
-    ro.modversion=$(LINEAGE_VERSION)
+    ro.lineage.releasetype=$(LINEAGE_BUILDTYPE)

FAOSPx01/vendor_faosp

config/version.mk
@@ -15,5 +15,4 @@ PRODUCT_SYSTEM_PROPERTIES += \
     ro.faosp.version=$(FAOSP_VERSION) \
     ro.faosp.display.version=$(FAOSP_DISPLAY_VERSION) \
     ro.faosp.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
-    ro.faosp.releasetype=$(FAOSP_BUILDTYPE) \
-    ro.modversion=$(FAOSP_VERSION)
+    ro.faosp.releasetype=$(FAOSP_BUILDTYPE)