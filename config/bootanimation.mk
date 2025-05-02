70c6bdf170c4598751476c11f5fa5ec60e7474a8
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-05-02 18:34:18 +0530

    vendor: Update bootanimation
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -1,15 +1,17 @@
-# Bootanimation
-ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
-     PRODUCT_COPY_FILES += vendor/faosp/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
-     PRODUCT_COPY_FILES += vendor/faosp/bootanimation/bootanimation_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
-     PRODUCT_COPY_FILES += vendor/faosp/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+# Check for 720p devices first
+ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+    PRODUCT_COPY_FILES += \
+        vendor/faosp/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+
 else
-    ifeq ($(TARGET_BOOT_ANIMATION_RES),)
-        $(warning "TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
-    else
-        $(warning "Current bootanimation res is not supported, forcing 1080p")
+    # Default to WHITE bootanimation
+    BOOTANIMATION_VARIANT := white
+
+    # Use BLACK if GEMENI bootanimation is enabled
+    ifeq ($(TARGET_HAS_GEMENI_BOOTANIMATION),true)
+        BOOTANIMATION_VARIANT := gemini
     endif
-    PRODUCT_COPY_FILES += vendor/faosp/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+
+    PRODUCT_COPY_FILES += \
+        vendor/faosp/bootanimation/bootanimation_$(BOOTANIMATION_VARIANT).zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 endif
